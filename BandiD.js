61f4570d5fd39975aa27ef57f77dca10b7850baa
tonmeister1959 <sean.e.olive@gmail.com>
2024-09-20 14:11:29 -0700

    Add files via upload

--- BandiD.js
+++ BandiD.js
@@ -6,24 +6,26 @@ var myanswer = ""
 var NumberofCorrectAnswers = 0
 var TrialsCompleted = 0
 var TotalTrials = 10
-var dbincrement = canvas.height-20 /30
+var dbincrement = canvas.height /30
 var NumberofBands = 2
 const centerFreq = [];
+
+document.getElementById("canvas").style.width = "600px"
+var canvasContent =  canvas.getContext('2d')
+canvasContent.width = "600px"
+//console.debug (document.getElementById("canvas").style.width)
+
+
 const data = [  
   //{ Trial: 0, NumberofBands: 0, CorrectBand: 0, MyAnswer: 0 }
-
   ]; 
 
 var starttime = Date.now()
 var LevelPercentage = 0  // have to achieve 80% in a level to advance
 var MinimumTrials = 0 // to to at least complete 5 trials to advance
 var CorrectAnswersInLevel = 0
-
-
-
+//componentDidMount()
 CorrectFilterFrequency = 0
-var oct  =   Math.log(20000/20) / Math.log(2)
-oct= 600 / oct
 const Freq= [];
 Freq[0] = 20
 Freq[1] = 50
@@ -38,43 +40,13 @@ Freq[9] = 20000
 
 
 
-const FrequencyLabels = [10];  // these are the filter frequencies
-FrequencyLabels[0] = "20"
-FrequencyLabels[1] = "  50"
-FrequencyLabels[2] = "  100"
-FrequencyLabels[3] = " 200"
-FrequencyLabels[4] = " 500"
-FrequencyLabels[5] = " 1k"
-FrequencyLabels[6] = " 2k"
-FrequencyLabels[7] =  "   5k"
-FrequencyLabels[8] =  "  10k"
-FrequencyLabels[9] =  " 20k"
-
-const dBLabel = [7]
- dBLabel[0] = "15"
-  dBLabel[1] = "10"
-  dBLabel[2] = "5"
-  dBLabel[3] = "0 dB"
-  dBLabel[4] = "-5"
-  dBLabel[5] = "-10"
-  dBLabel[6] = "-15"
 
 
-
-var canvasContext = canvas.getContext('2d')
-const ctx = document.getElementById("canvas").getContext("2d");
-  ctx.font = "15px Verdana"; 
-  for (let index = 0; index < FrequencyLabels.length; index++) {
-    //const element = array[index];
-    //ctx.fillText(FrequencyLabels[index], index * 60, 300)
-    
-  }
+var canvasContent = canvas.getContext('2d')
+//console.debug(document.getElementsByName('canvasContent').style.width)
 
 
-
-
-
-var nFreqs = 2000, // This defines the resolution of the frequency response; note that they are linearly spaced
+var nFreqs = 2000; // This defines the resolution of the frequency response; note that they are linearly spaced
     Freqs = new Float32Array(nFreqs),
     Mags = new Float32Array(nFreqs),
     Phases = new Float32Array(nFreqs)
@@ -83,21 +55,22 @@ for (var i=0;i<nFreqs;++i) Freqs[i] = 20000/nFreqs*(i+1)
 var biquadFilter = new BiquadFilterNode(audioContext,{frequency:1000,Q:1,gain:1})
 var CurrentFilter = "Peaking"
 
+//****** Create Buttons that Correspond to Filters on Graphs that Users Click */
 function CreateButtons(x,y,BandNumber) {
   const newButton = document.createElement('button');
   var currentbutton
- 
-  newButton.style.zIndex = 2
+ console.debug (x)
+//newButton.style.zIndex = "1"
   newButton.style.position = "absolute"
   newButton.style.top = y
   newButton.style.left = x
-  newButton.style.width = 25
+newButton.style.width = 25
   newButton.style.height = 25
   newButton.innerText = BandNumber
   newButton.id =  "Button" + BandNumber
 
   newButton.addEventListener('click',function(e) {
-    console.debug (this.style.backgroundColor)
+    
     
     this.style.backgroundColor = 'green'
 
@@ -106,83 +79,60 @@ handleclick(this.id)
 
   })
 document.body.appendChild(newButton)
+
 myanswer = ""
 
 }
   function handleclick(buttonclicked) {
-   
-    console.debug (buttonclicked)
+   //console.debug (buttonclicked.left)
 
   if (previousbutton == null) {
-  
-    previousbutton = buttonclicked
-    
+  previousbutton = buttonclicked
   }
     else {
-
       const elem = document.getElementById(previousbutton)
       elem.style.backgroundColor = ""
       console.debug ('previous button clicked ') + previousbutton.id
       previousbutton = buttonclicked
   //console.debug (previousbutton)
     }
-  
-  
-  
-  
-  }
-  
+    }
+  ///******* Draw Frequency Graph */
+    function DrawScale() {    
+    canvasContent.strokeStyle = 'black'
+        canvasContent.setLineDash([1, 2])
+        //canvasContent.setLineDash([0,0])
+       
+//console.log(document.getElementById('canvasContentContext').style.width)
 
-
-
-  
- 
-  
-
-  
-    function DrawScale() {
-        var x = 0
-        var x1 = 0
-        var y = 0
-        var y1 = 0
-        canvasContext.strokestyle = 'black'
-        canvasContext.setLineDash([2, 5])
-        canvasContext.setLineDash([0,0])
         for (let j =0;j <10; ++j) {
-          x =  Math.log(Freq[j]/Freq[0]) / Math.log(2) * oct
-      y = 0
-      y1 = 300
-      
-        canvasContext.moveTo(x+20,y)
-        canvasContext.lineTo(x+20,y1)
-        //canvasContext.beginPath()
-        canvasContext.stroke()
+         var a =  (Math.log(Freq[j]/20)) / Math.log(2) * 30 //28// 30.10299957
+         //a = a *
+         console.log(a) +  "  " + Freq[j]
+         
+         canvasContent.beginPath()
+        canvasContent.moveTo(a,0)
+      canvasContent.lineTo(a, 300)  /// draws the vertical lines on the graph
+        //canvasContentContent.beginPath()
+        canvasContent.stroke()
       }
    
-      // puts frequencies on graph
-      const ctx = document.getElementById("canvas").getContext("2d");
-      ctx.font = "15px verdana";
-      for (let index = 0; index < FrequencyLabels.length; index++) {
-        //const element = array[index];
-        ctx.fillText(FrequencyLabels[index], index * 63+20, 320)
-        
-
-      }
-      for (let index = 0; index < dBLabel.length; index++) {
-        console.debug (dBLabel[index])
-        canvasContext.fillText(dBLabel[index], 0, index * 50 + 15)
-        
-
-      }
       
+     
+       
     }
 
+    //************* Draw Filter on Graph*************  */
     function DrawFilters(NumberofFilters, Qfactor, GainindB, FilterTypes) {
       var NumberOctaves = 9.97
+       var Number = 1
+       canvasContent.beginPath
+//canvasContent.fillStyle = 'transparent'
+
+      canvasContent.clearRect(0,0,300,600)
+      //canvasContent.stroke()
+
       
-      
-      var Number = 1
-      canvasContext.clearRect(20,0,canvas.width,canvas.height)
       for (let index = 0; index < NumberofFilters; index++) {
         centerFreq[index] = 25 * 2** (9.97 * Number/(NumberofFilters * 2))
         //console.debug (centerFreq[index])
@@ -193,41 +143,45 @@ myanswer = ""
         biquadFilter.gain.value = GainindB
         biquadFilter.type = "peaking"
         biquadFilter.getFrequencyResponse(Freqs,Mags,Phases)
-        canvasContext.beginPath()
-        const half = canvas.height -65
-        console.debug (dbincrement * 30)
-      
-
-        canvasContext.strokeStyle = 'blue'
-        canvasContext.setLineDash([0,0])
-        for (let i=1;i<nFreqs;++i) { 
-          x =  Math.log(Freqs[i]/Freq[0]) / Math.log(2) * oct
-          
-
-          canvasContext.lineTo(x+20,half -Mags[i]*85)
-       //canvasContext.lineTo(x+20,canvas.height-Mags[i]*90)
-        canvasContext.stroke()
+        
+        const half = canvasContent.height / 2
+        canvasContent.strokeStyle = 'blue'
+        canvasContent.setLineDash([0,0])
+        canvasContent.beginPath()
+  
+        for (let i=0;i<nFreqs;++i) { 
+          x =  (Math.log(Freqs[i]/20)) / Math.log(2)  * 30// 30.10299957
+         canvasContent.lineTo(x,125 -Mags[i]*40)
+       //canvasContent.lineTo(x,canvasContent.height-Mags[i]*100)
+        canvasContent.stroke()
         }
         
        }
        
        for (let i=0;i <NumberofFilters;++i) {
-       x =  (Math.log(centerFreq[i]/20)/ (Math.log(2)) * oct)
-       console.log(x)
-      CreateButtons(x+20,150,i+1 )
+       x =  (Math.log(centerFreq[i]/20))/ (Math.log(2)) * 60 //30.10299957
+       console.debug (x + '  ' + centerFreq[i])
+       x = x + 35 // offset 
+       console.debug (x)
+      CreateButtons(x,145,i+1 )
       myanswer = ""
        }
-    canvasContext.strokeStyle = 'black'
+      
+    canvasContent.strokeStyle = 'black'
    
 const rand1 = Math.floor(Math.random() * NumberofFilters)
 console.debug (rand1)
+var x= canvasContent.left
+var y = canvasContent.top 
 
    biquadFilter.frequency.value = centerFreq[rand1]
    CorrectFilterFrequency =  biquadFilter.frequency.value
    correctanswer = rand1 + 1
-   canvasContext.beginPath()
-   canvasContext.rect(20,0,600,300)
-   canvasContext.stroke()
+   canvasContent.beginPath()
+   //canvasContent.strokerect(20,0,300,600)
+   //canvasContent.strokerect(20,0,300,600)
+   //canvasContent.stroke()
+   canvasContent.position = "absolute"
   }
 
 
@@ -265,7 +219,7 @@ if (WhichOne ==2) {
   // Give feedback on answers
   if (myanswer == correctanswer) {
    var EndTime = Date.now()
-    alert ('You are correct! The Band is: ' + correctanswer +  ' ( ' +  Math.floor(CorrectFilterFrequency) + ' Hz )')
+    alert ('You are correct! The Band is: ' + correctanswer +  ' ( ' +  Math.random(CorrectFilterFrequency) + ' Hz )')
     CorrectAnswersInLevel =CorrectAnswersInLevel + 1
     MinimumTrials = MinimumTrials + 1
    
@@ -286,7 +240,7 @@ if (WhichOne ==2) {
    
     console.debug (percentage)
     Trials.innerText = "Trials: " + TrialsCompleted// + " of " + TotalTrials
-    correct.innerText = "Correct Answers: " +  Math.trunc(percentage) + " %"
+    correct.innerText = "Correct Answers: " +  (percentage.toFixed(1)) + " %"
  
     
   }
@@ -298,7 +252,8 @@ if (WhichOne ==2) {
    generateTable(data)
    
    function generateTable(data) {  
-    let table = '<table>';  
+    let table = '<table>'; 
+    
    
     table += '<tr><th>Trial</th><th>NumberofBands</th><th>CorrectBand</th><th>MyAnswer</th><th>CorrectFreq</th><th>MyFreq</th><th>Response time (s)</th></tr>';  
     data.forEach(item => {  
@@ -368,3 +323,28 @@ start.onclick = () => {
   
   
 }
+function loadmenu() {
+  document.getElementById("menu").innerHTML =  '<object type="text/html" data = "http://localhost/HowtoListen/CurtainMenu.html" style="width:100%; height:100%;overflow:visible;" ></object>'
+  console.debug (menu.innerHTML)
+   //Document.getElementById("menu").style.display="block" 
+  menu.style.display = "block"
+  
+}
+function openNav() {
+  document.getElementById("myNav").style.height = "70%";
+
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+
+}
+
+function NextTrial() {
+  
+    closeNav()
+
+  }
+  const urlParams = new URLSearchParams(window.location.search);
+  const greetingValue = urlParams.get('greeting');
+  //console.log(greetingValue); 
